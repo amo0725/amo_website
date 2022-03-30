3a1720f59fd968d86b63bec0d0f447de6e1afc2e
amo0725 <amo880725@gmail.com>
2022-03-30 20:49:56 +0800

    Fix meta tags

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,12 @@ const Header = () => {
     return (
         <>
             <link rel="icon" href="/favicon.ico"/>
+            <meta property="og:type" content="website"></meta>
             <meta name="image" property="og:image" content="/myWebsite.png"></meta>
+            <meta property="og:image:width" content="512" />
+            <meta property="og:image:height" content="320" />
             <meta name="description" property="og:description" content="Amo Feng's Personal Website - I'm a Full-Stack Developer with a bachelor of science in Computer Science & Information Engineering from Tamkang University . Currently working at IDCtech in Taiwan."></meta>
             <meta name="author" content="Amo Feng"></meta>
-            <meta property="og:type" content="Website"></meta>
         </>
     )
 }
